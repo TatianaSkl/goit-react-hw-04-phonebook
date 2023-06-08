62ee03847e5f52a470895273c004c073b8ceae55
TatianaSkl <116264242+TatianaSkl@users.noreply.github.com>
2023-06-08 09:32:49 +0300

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
+import { ToastContainer, toast } from 'react-toastify';
 import { Container, Section, ContactForm, ContactList, Filter, Heading } from 'components';
 
 const LS_KEY = 'contactsLS';
 
 export const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    return JSON.parse(localStorage.getItem(LS_KEY)) ?? [];
-  });
+  const [contacts, setContacts] = useState(() => JSON.parse(localStorage.getItem(LS_KEY)) ?? []);
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
@@ -21,15 +20,19 @@ export const App = () => {
   const handleAddContact = (name, number) => {
     const isExist = contacts.find(contact => contact.name.toLowerCase() === name.toLowerCase());
     if (isExist) {
-      alert(`${name} is already in contacts.`);
+      toast.warn(`${name} is already in contacts.`);
       return;
     }
     const newContact = { id: nanoid(), name, number };
     setContacts(prevContacts => [...prevContacts, newContact]);
+    toast.success(`${name} is added to the contact list!`);
+    return;
   };
 
   const handleDeleteContact = id => {
     setContacts(prevContacts => prevContacts.filter(contact => contact.id !== id));
+    toast.info(`The contact has been deleted!`);
+    return;
   };
 
   const getFilteredContacts = () => {
@@ -41,6 +44,7 @@ export const App = () => {
 
   return (
     <Container>
+      <ToastContainer autoClose={3000} />
       <Section>
         <Heading>Phonebook</Heading>
         <ContactForm onAddContact={handleAddContact} />
